6516729822c3fcd39f359b903d83f47bba944f7d
Akanle Jeremy <akanlejeremy@gmail.com>
2025-02-18 12:55:49 +0300

    further configuration and initial signup endpoint skeleton

--- server/app.py
+++ server/app.py
@@ -1,8 +1,8 @@
-from flask import Flask
+from flask import Flask, make_response, request, session
 from flask_bcrypt import Bcrypt
 from flask_cors import CORS
 from flask_migrate import Migrate
-from flask_restful import Api
+from flask_restful import Api, Resource
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 from config import Config
@@ -11,24 +11,44 @@ from config import Config
 db = SQLAlchemy(metadata=MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
 }))
-bcrypt = Bcrypt()
-migrate = Migrate()
-cors = CORS()
-api = Api()
 
-def create_app():
-    app = Flask(__name__)
-    app.config.from_object(Config)
+app = Flask(__name__)
+app.config.from_object(Config)
 
-    # Link extensions to app
-    db.init_app(app)
-    bcrypt.init_app(app)
-    migrate.init_app(app, db)
-    cors.init_app(app, supports_credentials=True)
-    api.init_app(app)
+bcrypt = Bcrypt(app)
 
-    return app
+migrate = Migrate(app, db)
+cors = CORS(app, supports_credentials=True)
+api = Api(app)
 
+db.init_app(app)
+
+class Signup(Resource):
+    def post(self):
+        pass
+        # data = request.get_json()
+        
+        # print("Received data: ", data)
+        # user = User(
+        #     username=data.get('username'), 
+        #     email=data.get('email')
+        # )
+        # user.password_hash = data.get('password')
+        
+        # try:
+        #     db.session.add(user)
+        #     db.session.commit()
+            
+        #     session['user_id'] = user.id
+        #     print(session)
+        #     return make_response(user.to_dict(), 201)
+        # except Exception:
+        #     return make_response({"Error": "Error signing up. Check credentials."}, 422)
+        
+        
+
+api.add_resource(Signup, '/signup')
+
+# print(app.url_map)
 if __name__ == '__main__':
-    app = create_app()
     app.run(port=5000, debug=True)
