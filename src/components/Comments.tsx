034311fa32e86f9b5c0aaed21b44e9973dc53e60
Mboya72 <elvindio72@gmail.com>
2025-02-21 08:09:15 +0300

    SCRUM-3 adding a feed

--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -1,4 +1,4 @@
-import Image from "./Image"
+import Image from "next/image"
 import Post from "./Post"
 
 const Comments = () => {
@@ -6,7 +6,7 @@ const Comments = () => {
     <div className=''>
       <form className='flex items-center justify-between gap-4 p-4 '>
         <div className='relative w-10 h-10 rounded-full overflow-hidden'>
-          <Image path="general/avatar.png" alt="Lama Dev" w={100} h={100} tr={true}/>
+          <Image src="general/avatar.png" alt="Elvizz" width={100} height={100} />
         </div>
         <input type="text" className="flex-1 bg-transparent outline-none p-2 text-xl" placeholder="Post your reply"/>
         <button className="py-2 px-4 font-bold bg-white text-black rounded-full">Reply</button>
